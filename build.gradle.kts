604759f7719ebe63932fd4fb6ef51fb51488a5d8
Matthew <matthewhjt72@gmail.com>
2024-05-25 06:40:45 +0700

    feat: add spring security and jwt token dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
