001d6e56124606f7e6a5a4cfaae8cdf7291360dc
Matthew <matthewhjt72@gmail.com>
2024-05-26 01:55:25 +0700

    refactor: make User.username unique

--- src/main/java/hoomgroom/auth/model/User.java
+++ src/main/java/hoomgroom/auth/model/User.java
@@ -23,6 +23,8 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String firstname;
     private String lastname;
